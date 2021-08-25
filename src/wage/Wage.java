d764348a20abb1b04505051c6fd5f32479e663f9
RagulKanna <ragulragi619@gmail.com>
2021-08-25 23:51:42 +0530

    Added: full employee salary

--- src/wage/Wage.java
+++ src/wage/Wage.java
@@ -2,9 +2,13 @@ package wage;
 
 public class Wage {
     public static void main(String[] args) {
-        Emppresentorabsent();
+      //  Emppresentorabsent();
+        FullTimeEmpsalary();
     }
-    public static void Emppresentorabsent(){
+
+
+
+   /* public static void Emppresentorabsent(){
         int IsPresent = 1;
         int EmpCheck = (int) Math.floor(Math.random() * 10 % 2);
         System.out.println("The value of Attendance: " + EmpCheck);
@@ -12,6 +16,26 @@ public class Wage {
             System.out.println("So The Employee is Present Today");
         else
             System.out.println("So The Employee is Absent Today");
-    }
+    }*/
+   public static void FullTimeEmpsalary()
+   {
+
+
+       int FullEmployee = 8;
+       int femployee = (int) (Math.floor(Math.random() * 10) % 2);
+
+
+       int Fsalary = payforhour * FullEmployee;
+
+
+       if (femployee == 1) {
+           System.out.println("Employee's one day Salary is " + Fsalary);
+
+
+       } else
+           System.out.println("No salary if they Absent");
+
+
+   }
 
 }
